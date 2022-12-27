bbd6c40f8cc506f15cfb8ec7a1ebb1a0c5fab990
User <User@DESKTOP-33QRJMP>
2022-12-27 15:22:22 +0530

    build 102

---
+++ AdvSel/src/Assignment/Assi.java
@@ -0,0 +1,36 @@
+package Assignment;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assi {
+	
+	public static void main(String[] args) throws AWTException, InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "./Software/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		Thread.sleep(2000);
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		Dimension d = new Dimension(500, 500);
+		Thread.sleep(2000);
+		driver.manage().window().setSize(d);
+		Thread.sleep(2000);
+		Robot r = new Robot();
+		r.keyPress(KeyEvent.VK_ALT);
+		r.keyPress(KeyEvent.VK_SPACE);
+		r.keyRelease(KeyEvent.VK_ALT);
+		r.keyRelease(KeyEvent.VK_SPACE);
+		Thread.sleep(2000);
+		r.keyPress(KeyEvent.VK_N);
+		r.keyRelease(KeyEvent.VK_N);
+		
+		
+	}
+}
